928e3c4205a7e697b2538f89048b0a7d144bf0d0
Diky Arga <dikyarga.id@gmail.com>
2017-02-10 18:58:16 +0700

    refactore the code

--- flashcards.js
+++ flashcards.js
@@ -5,94 +5,118 @@ let readline = require('readline')
 var readlineSync = require('readline-sync');
 
 class Model {
-  constructor() {
-    this.fileName = 'data.json'
-    this.questionList = []
-  }
+    constructor() {
+        this.fileName = 'data.json'
+        this.questionList = []
+    }
 
-  parseFile(){
-    debugger
-    this.questionList = jsonfile.readFileSync(this.fileName)
-  }
+    parseFile() {
+        debugger
+        this.questionList = jsonfile.readFileSync(this.fileName)
+    }
 
-  writeToFile(){
-    jsonfile.writeFileSync(this.fileName, this.todoList)
-  }
+    writeToFile() {
+        jsonfile.writeFileSync(this.fileName, this.todoList)
+    }
 
-  getQuestion(index){
-    return this.questionList[index].definition
-  }
+    getQuestion(index) {
+        console.log(this.questionList[index].definition);
+        return this.questionList[index].definition
+    }
 
-  getAnswer(index){
-    return this.questionList[index].term
-  }
+    getAnswer(index) {
+        // console.log(this.questionList[index].term);
+        return this.questionList[index].term
+        // return 'laflsf'
+    }
 
-  get question(){
-    // console.log(this.questionList[index]);
-    // return this.questionList[index].definition
-  }
+    get question() {
+        // console.log(this.questionList[index]);
+        // return this.questionList[index].definition
+    }
 
-  get answer(){
-    // console.log(this.questionList[index].term);
-    // return this.questionList[index].term
-  }
+    get answer() {
+        // console.log(this.questionList[index].term);
+        // return this.questionList[index].term
+    }
 
-  get lengthOfQuestions(){
-    return this.questionList.length
-  }
+    lengthOfQuestions() {
+        return this.questionList.length - 1
+    }
 }
 
 class View {
-  constructor() {
+    constructor() {
 
-  }
+    }
 
-  showQuestion(questionList, index){
-    console.log(questionList[index].definition);
-  }
+    showQuestion(questionList, index) {
+        console.log(questionList[index].definition);
+    }
 
-  wrongAnswer(){
-    console.log(`Jawaban mu salah, Sis! Coba lagi yak!`);
-  }
+    wrongAnswer() {
+        console.log(`Jawaban mu salah, Sis! Coba lagi yak!`);
+    }
 
-  correctAnswer(){
-    console.log(`Jawaban kamu benar, Sis!`);
-  }
+    correctAnswer() {
+        console.log(`Jawaban kamu benar, Sis!`);
+    }
 
-  welcomeMessage(){
-    console.log('----==== Selamat datang di Game Flash Card ===-----');
-  }
+    welcomeMessage() {
+        console.log('----==== Selamat datang di Game Flash Card ===-----');
+    }
 
-  greating(){
-    console.log('-----------------------');
-    console.log('Selamat kamu menang, Sis!');
-  }
+    greating() {
+        console.log('-----------------------');
+        console.log('Selamat kamu menang, Sis!');
+    }
 }
 
 class Controller {
-  constructor() {
-    this.model = new Model()
-    this.view = new View()
-    this.arguments = process.argv
-  }
+    constructor() {
+        this.model = new Model()
+        this.view = new View()
+        this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            prompt: 'Aloha > '
+        });
+    }
 
-  init(){
-    this.model.parseFile()
-    this.view.welcomeMessage()
+    init() {
+        this.model.parseFile()
+        this.view.welcomeMessage()
 
-    let index = 0
-    do {
-      let answer = readlineSync.question(this.model.getQuestion(index));
-      if (answer.toLowerCase() == this.model.getAnswer(index).toLowerCase()) {
-        this.view.correctAnswer()
-        index++
-      } else {
-        this.view.wrongAnswer()
-      }
-    } while (index < this.model.lengthOfQuestions);
+        let index = 0
+        this.model.getQuestion(index)
+        // Versi Async
+        this.rl.prompt()
+        this.rl.on('line', (answer) => {
+            if (this.model.getAnswer(index).toLowerCase() == answer.toLowerCase()) {
+                this.view.correctAnswer()
+                index++
+                if (index >= this.model.lengthOfQuestions()) {
+                    this.view.greating()
+                    return this.rl.close()
+                }
+            } else {
+                this.view.wrongAnswer()
+            }
+            this.model.getQuestion(index)
+        })
 
-    this.view.greating()
-  }
+        // Versi Syncronous
+        // do {
+        //   let answer = readlineSync.question(this.model.getQuestion(index));
+        //   if (answer.toLowerCase() == this.model.getAnswer(index).toLowerCase()) {
+        //     this.view.correctAnswer()
+        //     index++
+        //   } else {
+        //     this.view.wrongAnswer()
+        //   }
+        // } while (index < this.model.lengthOfQuestions);
+
+    }
 }
 
 let playMVC = new Controller()
